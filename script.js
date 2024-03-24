ab0f3ecfc9886faa8e5eca1cc39084052697c8ac
“Abraham” <AbrahhamDev@users.noreply.github.com>
2024-03-24 13:26:29 -0500

    My first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// let boton = document.getElementById("miBoton");
+// boton.addEventListener("click", function() {
+//     alert("¡Haz hecho click en el boton!");
+// });
+
+// let formulario = document.getElementById("miFormulario");
+// formulario.addEventListener("submit", function(evento) {
+//     evento.preventDefault();
+//     alert("Formulario enviado!");
+// });
